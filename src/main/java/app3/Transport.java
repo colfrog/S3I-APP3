c35e45c5cc6321db3941b3b6a7936748a02d5746
Ced <few>
2020-10-25 20:50:15 -0400

    Finished Transport Send Function

--- src/main/java/app3/Transport.java
+++ src/main/java/app3/Transport.java
@@ -1,9 +1,36 @@
 package app3;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Transport extends CoucheProto {
     public void send(String data) throws IOException {
+
+        final int charPerPaquet = 100;
+
+        if(data.length()==0){
+            return;
+        }
+        List<String> dividedData = new ArrayList<String>();
+
+        //dividedData.add("\0\0\0");
+
+        int index = 0;
+        while (index < data.length()) {
+            dividedData.add(data.substring(index, Math.min(index + charPerPaquet,data.length())));
+            index += charPerPaquet;
+        }
+        //dividedData.add("/0/0/0");
+        for(int i=0;i<dividedData.size();i++){
+            nextSend.send(dividedData.get(i));
+        }
+
+
+
+
+
         // data contient un fichier complet. Sépare en paquets de 200 octets et envoie à nextSend.
     }
 
